c3168d76c817dd4437d1ff3479dfee5c633bfcf0
DiogoOliveiraZup <didioliverf1@hotmail.com>
2017-03-02 17:58:15 -0300

    new files

--- SetupInfosiRacing/app/src/main/java/com/diogooliveira/setupinfo/setupinfosiracing/MainActivity.java
+++ SetupInfosiRacing/app/src/main/java/com/diogooliveira/setupinfo/setupinfosiracing/MainActivity.java
@@ -5,12 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +27,18 @@ public class MainActivity extends AppCompatActivity {
     @Bind(R.id.damper) RadioButton spring;
     @Bind(R.id.differential) RadioButton differential;
 
+
+    // MP4-30 Configs //
+    @Bind(R.id.tires) RadioButton tires;
+    @Bind(R.id.aeropackage) RadioButton aeropackage;
+    @Bind(R.id.aerocalculator) RadioButton aerocalculator;
+    @Bind(R.id.frontchassi) RadioButton frontchassi;
+    @Bind(R.id.frontleftrightchassi) RadioButton frontleftrightchassi;
+
+
+
+    @Bind(R.id.backbutton)ImageButton backButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,11 +46,16 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        backButton.setVisibility(View.GONE);
 
             configRadio.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
 
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                principalText.setVisibility(View.VISIBLE);
+                configRadio.setVisibility(View.GONE);
+                backButton.setVisibility(View.VISIBLE);
 
                 if (checkedId == toeInOut.getId()){
                     titleText.setText(R.string.toeintoeouttitle);
@@ -57,9 +77,39 @@ public class MainActivity extends AppCompatActivity {
                     titleText.setText(R.string.differentialtitle);
                     principalText.setText(Html.fromHtml(getResources().getString(R.string.differentialtext)));
                 }
+                // MP4-30//
+                if (checkedId == tires.getId()){
+                    titleText.setText(R.string.tiresmp430title);
+                    principalText.setText(Html.fromHtml(getResources().getString(R.string.tiresmp430text)));
+                }
+                if (checkedId == aeropackage.getId()){
+                    titleText.setText(R.string.aeropackagemp430title);
+                    principalText.setText(Html.fromHtml(getResources().getString(R.string.aeropackagemp430text)));
+                }
+                if (checkedId == aerocalculator.getId()){
+                    titleText.setText(R.string.aerocalculatormp430title);
+                    principalText.setText(Html.fromHtml(getResources().getString(R.string.aerocalculatormp430text)));
+                }
+                if (checkedId == frontchassi.getId()){
+                    titleText.setText(R.string.frontchassimp430title);
+                    principalText.setText(Html.fromHtml(getResources().getString(R.string.frontchassimp430text)));
+                }
+                if (checkedId == frontleftrightchassi.getId()){
+                    titleText.setText(R.string.frontleftandrightchassimp430title);
+                    principalText.setText(Html.fromHtml(getResources().getString(R.string.frontleftandrightchassimp430text)));
+                }
 
             }
         });
 
     }
+
+    @OnClick(R.id.backbutton)
+    public void CloseText(){
+        backButton.setVisibility(View.GONE);
+        principalText.setVisibility(View.GONE);
+        configRadio.setVisibility(View.VISIBLE);
+        titleText.setText("");
+    }
+
 }
